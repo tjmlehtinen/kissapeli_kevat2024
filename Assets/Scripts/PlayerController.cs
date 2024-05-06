efaa87c1f6e6435fe13831180d32b54ff48ce15d
Tommi Lehtinen <tommijmlehtinen@gmail.com>
2024-05-06 18:19:40 +0300

    kissa liikkuu

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,9 @@ public class PlayerController : MonoBehaviour
 {
     public CharacterController controller;
     public float speed = 3f;
-    private float vertikaaliNopeus = 0;
+    private float verticalInput = 0;
+    private float horizontalInput = 0;
+    private float mouseInput = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +18,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 direction = new Vector3(horizontalInput, -1, verticalInput);
+        controller.Move(direction * speed * Time.deltaTime);
+
+        mouseInput = Input.GetAxis("Mouse X");
     }
 }
